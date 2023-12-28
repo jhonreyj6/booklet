81241c1a9a04a2c2be6fe27baf34bf18f3849ce2
ATG <j6cafe2018@gmail.com>
2023-12-28 13:38:48 +0800

    save

--- database/migrations/2023_12_28_053316_create_books_table.php
+++ database/migrations/2023_12_28_053316_create_books_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('profile_img');
-            $table->rememberToken();
+            $table->string('description');
+            $table->string('author');
+            $table->integer('price');
+            $table->integer('stocks');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('books');
     }
 };
